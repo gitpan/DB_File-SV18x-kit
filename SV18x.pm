60b6c7a0ff41ca8f6ad27715549fc3e1c434328c
Andreas J. König <andreas.koenig.7os6vvqr@franz.ak.mind.de>
1999-12-04 07:02:57 +0000

    Import of ANDK/DB_File-SV18x-kit-0.05 from CPAN.
    gitpan-cpan-distribution: DB_File-SV18x-kit
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         ANDK/DB_File-SV18x-kit-0.05.tar.gz
    gitpan-cpan-author:       ANDK
    gitpan-cpan-maturity:     released

--- SV18x.pm
+++ SV18x.pm
@@ -1,5 +1,5 @@
 package DB_File::SV18x;
-$VERSION="0.04";
+$VERSION="0.05";
 
 die "DB_File::SV18x is a dummy package without functionality.
 Please try DB_File::SV185 or DB_File::SV186, whatever has been
@@ -9,7 +9,7 @@ __END__
 
 =head1 NAME
 
-DB_File::SV18x - Lets DB_File for old and new db coexist
+DB_File::SV18x - Co-existence of berkeley db 1.85, 1.86 and 2+
 
 =head1 SYNOPSIS
 
@@ -19,10 +19,10 @@ Identical to DB_File
 
 The DB_File::SV18x modules override the namespace used by the berkeley
 db library with a namespace that allows the most prominent versions of
-the library, namely 1.85 and 1.86, coexist with the current 2.x in
-memory. Thus it offers both convenient transformations of database
-files between different versions and allows a smooth upgrade path from
-1.8x to 2.0.
+the library, namely 1.85 and 1.86, coexist with the current 2 or
+higher in memory. Thus it offers both convenient transformations of
+database files between different versions and allows a smooth upgrade
+path from 1.8x to 2.0.
 
 For usage information please consult the documentation for DB_File and
 globally replace the token C<DB_File> by C<DB_File::SV185> or
@@ -33,19 +33,19 @@ C<DB_File::SV186> whatever is avilable on your system.
   use DB_File ();
   use DB_File::SV185 ();
   use Fcntl;
-  $F = "str.db";   
+  $F = "str.db";
   tie(%h, 'DB_File', "$F.200",
       O_RDWR|O_CREAT, 0644, $DB_File::DB_HASH) or die; 
   tie(%i, 'DB_File::SV185', $F,
       O_RDONLY, 0644, $DB_File::SV185::DB_HASH) or die; 
-  %h = %i;                                                    
+  %h = %i;
 
 This example does a conversion of a database file from 1.85 to
 whatever is the current default in the DB_File module of the machine
 that runs this code.
 
 Note that berkeley db 2.0 comes with excellent conversion tools and
-for mere conversion DB_File::SV18x is not necessary. It's strenth
+for mere conversion DB_File::SV18x is not necessary. Its usefulness
 lies in the open coexistence.
 
 =head1 AUTHOR
